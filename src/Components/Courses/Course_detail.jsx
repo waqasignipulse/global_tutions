b00c4f9c881fe1c9fbb0610b6c9e22717abbac85
waqasignipulse <waqas.ignipulse@gmail.com>
2024-11-28 16:13:32 +0500

    detail_pages_created

--- src/Components/Courses/Course_detail.jsx
+++ src/Components/Courses/Course_detail.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Course_detail_banner from '../Main/Course_detail_banner'
+import What_you_will_Learn from '../Main/What_you_will_Learn'
+import Subscribe from '../Main/Subscribe'
 
 const Course_detail = () => {
   return (
     <div>
       <Course_detail_banner/>
+      <What_you_will_Learn/>
+      <Subscribe/>
     </div>
   )
 }
